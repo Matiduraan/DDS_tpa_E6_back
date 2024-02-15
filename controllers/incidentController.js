a41625efec091ec8e01f14b8c1809f5b25110e4a
Matias Duran <matiduraan@gmail.com>
2024-02-14 21:21:18 -0300

    Add extra info to incidents

--- controllers/incidentController.js
+++ controllers/incidentController.js
@@ -4,7 +4,26 @@ const prisma = new PrismaClient();
 
 export async function getAllIncidents() {
   try {
-    return await prisma.incidente.findMany();
+    return await prisma.incidente.findMany({
+      include: {
+        comunidad: true,
+        servicio: true,
+        usuarioCierre: {
+          select: {
+            username: true,
+            puntosConfianza: true,
+            id: true,
+          },
+        },
+        usuarioInformador: {
+          select: {
+            username: true,
+            puntosConfianza: true,
+            id: true,
+          },
+        },
+      },
+    });
   } catch (error) {
     console.log(error);
     return [];
